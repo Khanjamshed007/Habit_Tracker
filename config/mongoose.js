1de2584cb4e7a8e4c9f88cbb9bcde1f3ecd2107c
jamshed khan <khanjamshed404@gmail.com>
2023-03-02 13:00:33 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose=require('mongoose');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_URL)
+
+
+const db=mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error to connect with mongodb"))
+
+db.once('open',()=>{
+    console.log('Connected to Database:mongodb')
+})
+
+module.exports=db;
